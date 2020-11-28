bd209651979074761df754ef599ac477f5117c68
Tech School <techschool.guru@gmail.com>
2020-11-28 13:43:37 +0100

    try timezone zulu

--- db/migration/000002_add_users.up.sql
+++ db/migration/000002_add_users.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE "users" (
   "hashed_password" varchar NOT NULL,
   "full_name" varchar NOT NULL,
   "email" varchar UNIQUE NOT NULL,
-  "password_changed_at" timestamptz NOT NULL DEFAULT('0001-01-01 00:00:00+00'),  
+  "password_changed_at" timestamptz NOT NULL DEFAULT('0001-01-01 00:00:00Z'),  
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
