cb9c3a185f4823eaa008b98e5b6d2741336a8dbb
Tech School <techschool.guru@gmail.com>
2021-01-13 20:15:26 +0100

    create a good payload for test

--- token/jwt_maker_test.go
+++ token/jwt_maker_test.go
@@ -48,14 +48,17 @@ func TestExpiredJWTToken(t *testing.T) {
 }
 
 func TestInvalidJWTTokenAlgNone(t *testing.T) {
-	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, &Payload{})
+	payload, err := NewPayload(util.RandomOwner(), time.Minute)
+	require.NoError(t, err)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
 	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
 	require.NoError(t, err)
 
 	maker, err := NewJWTMaker(util.RandomString(32))
 	require.NoError(t, err)
 
-	payload, err := maker.VerifyToken(token)
+	payload, err = maker.VerifyToken(token)
 	require.Error(t, err)
 	require.EqualError(t, err, ErrInvalidToken.Error())
 	require.Nil(t, payload)
