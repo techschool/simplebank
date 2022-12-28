6904a14cf6acd36cd8eb7268ab78bb794c0bbb8e
cheqrouni <a.cheqrouni@gmail.com>
2022-12-28 23:06:53 +0100

    Equal is able to compare two structs.

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -43,11 +43,7 @@ func TestGetAccount(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEmpty(t, account2)
 
-	require.Equal(t, account1.ID, account2.ID)
-	require.Equal(t, account1.Owner, account2.Owner)
-	require.Equal(t, account1.Balance, account2.Balance)
-	require.Equal(t, account1.Currency, account2.Currency)
-	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+	require.Equal(t, account1, account2)
 }
 
 func TestUpdateAccount(t *testing.T) {
