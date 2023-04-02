81749acc25ed22b83088ce398e53807313aba6ab
techschool <58562849+techschool@users.noreply.github.com>
2023-04-02 10:44:38 +0200

    add unit tests for create_user_rpc (#87)
    Co-authored-by: phamlequang <phamlequang@gmail.com>

---
+++ gapi/main_test.go
@@ -0,0 +1,23 @@
+package gapi
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	db "github.com/techschool/simplebank/db/sqlc"
+	"github.com/techschool/simplebank/util"
+	"github.com/techschool/simplebank/worker"
+)
+
+func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store, taskDistributor)
+	require.NoError(t, err)
+
+	return server
+}
