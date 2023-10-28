6e5229df22098b019248c9c01ae6f569783cf839
Quang Pham <phamlequang@gmail.com>
2023-10-28 16:58:56 +0200

    deploy grpc to eks

---
+++ eks/install.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/aws/deploy.yaml
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.1/cert-manager.yaml
